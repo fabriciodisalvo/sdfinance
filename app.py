fd401a09978941b2ddd6913d90b745761399ccbf
fabriciodisalvo <fabriciodisalvo@gmail.com>
2021-01-18 20:40:23 -0300

    Add Expense View page
    Added Expense view page, and database functionality
    in app.py to parse expenses from main.db

--- app.py
+++ app.py
@@ -12,3 +12,14 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route('/')
 def index():
     return render_template('index.html', title='Index')
+
+@app.route('/list')
+def list_expense():
+    with sqlite3.connect("main.db") as connection:
+        cursor = connection.cursor()
+        table_list_data = cursor.execute("SELECT * FROM EXPENSES ORDER BY ExpenseDate")
+    return render_template('expense_list.html', title='Expenses', table_list_data=table_list_data)
+
+@app.route('/new')
+def add_expense():
+    return render_template('expense_add.html', title='Add Expense')
