5e07946d63b508a330348146d8901eff6e53bc33
fabriciodisalvo <fabriciodisalvo@gmail.com>
2021-01-24 16:26:02 -0300

    Add View Expense page.
    All links on Expense List page will create dynamic links to
    <source>/view/<expense_id> which will parse the correct expense and
    show it as a card.
    Created a helpers file that formats the amount to be shown as needed
    by the formatted card.
    Temporarily changed image to static folder to access it.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask
 from flask import render_template, request, redirect
+from helpers import format_amount_view_expense
 
 
 # Configure application
@@ -17,12 +18,22 @@ def index():
 # Create List Expenses site
 @app.route('/list')
 def list_expense():
-    query = "SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId ORDER BY ExpenseDate"
+    query = "SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount, ExpenseId FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId ORDER BY ExpenseDate"
     with sqlite3.connect("main.db") as connection:
         cursor = connection.cursor()
         table_list_data = cursor.execute(query)
     return render_template('expense_list.html', title='Expenses', table_list_data=table_list_data)
 
-# @app.route('/new')
-# def add_expense():
-#     return render_template('expense_add.html', title='Add Expense')
+# Create View Single Expenses site,
+# as per https://pythonise.com/series/learning-flask/generating-dynamic-urls-with-flask
+@app.route("/view/<expense_id>")
+def profile(expense_id):
+    query = f"SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount, ExpenseId FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId WHERE ExpenseId={expense_id}"
+    with sqlite3.connect("main.db") as connection:
+        cursor = connection.cursor()
+        table_list_data = cursor.execute(query)
+        for data in table_list_data:
+            expense_data = [data[0], data[1], data[2], data[3], format_amount_view_expense(data[4])]
+            break
+        # Falta crear una funcion que traiga los tres gastos mas relevantes para mostrar debajo del gasto principal...
+    return render_template("expense_view.html", expense_data=expense_data)
