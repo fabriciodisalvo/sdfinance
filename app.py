d816ea267e3203af82c9e6b9fba6e980423b08b2
fabriciodisalvo <fabriciodisalvo@gmail.com>
2021-01-29 09:08:17 -0300

    Refactor sqlite3 database access

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import render_template, request, redirect, flash
 from helpers import format_amount_view_expense
 
 
+# Set up database
+connection = sqlite3.connect("main.db", check_same_thread=False)
+cursor = connection.cursor()
+
 # Configure application
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -20,9 +24,7 @@ def index():
 @app.route('/list')
 def list_expense():
     query = "SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount, ExpenseId FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId ORDER BY ExpenseDate"
-    with sqlite3.connect("main.db") as connection:
-        cursor = connection.cursor()
-        table_list_data = cursor.execute(query)
+    table_list_data = cursor.execute(query)
     return render_template('expense_list.html', title='Expenses', table_list_data=table_list_data)
 
 # Create View Single Expenses site,
@@ -30,33 +32,27 @@ def list_expense():
 @app.route("/view/<expense_id>")
 def profile(expense_id):
     query = f"SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount, ExpenseId FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId WHERE ExpenseId={expense_id}"
-    with sqlite3.connect("main.db") as connection:
-        cursor = connection.cursor()
-        table_list_data = cursor.execute(query)
-        for data in table_list_data:
-            expense_data = [data[0], data[1], data[2], data[3], format_amount_view_expense(data[4])]
-            break
+    table_list_data = cursor.execute(query)
+    for data in table_list_data:
+        expense_data = [data[0], data[1], data[2], data[3], format_amount_view_expense(data[4])]
+        break
         # Falta crear una funcion que traiga los tres gastos mas relevantes para mostrar debajo del gasto principal...
     return render_template("expense_view.html", expense_data=expense_data)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "GET":
-        with sqlite3.connect("main.db") as connection:
-            cursor = connection.cursor()
-            categories_data = list(cursor.execute("SELECT CategoryId, CategoryName FROM CATEGORIES"))
-            accounts_data = list(cursor.execute("SELECT AccountId, AccountName FROM ACCOUNTS"))
+        categories_data = list(cursor.execute("SELECT CategoryId, CategoryName FROM CATEGORIES"))
+        accounts_data = list(cursor.execute("SELECT AccountId, AccountName FROM ACCOUNTS"))
         return render_template("expense_add.html", categories_data=categories_data, accounts_data=accounts_data)
     else:
-        with sqlite3.connect("main.db") as connection:
-            cursor = connection.cursor()
-            description = request.form.get("description")
-            amount = float(request.form.get("amount")) * -1
-            category = request.form.get("category")
-            account = request.form.get("account")
-            date = '2020.01.25'
-            query = f"INSERT INTO EXPENSES (ExpenseDate, ExpenseDescription, CategoryId, AccountId, ExpenseAmount) VALUES (?, ?, ?, ?, ?)"
-            cursor.execute(query, (date, description, category, account, amount))
-
+        description = request.form.get("description")
+        amount = float(request.form.get("amount")) * -1
+        category = request.form.get("category")
+        account = request.form.get("account")
+        date = '2020.01.25'
+        query = f"INSERT INTO EXPENSES (ExpenseDate, ExpenseDescription, CategoryId, AccountId, ExpenseAmount) VALUES (?, ?, ?, ?, ?)"
+        cursor.execute(query, (date, description, category, account, amount))
+        connection.commit()
         flash('Expense added')
         return redirect("/list")
