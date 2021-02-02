806b002534caff335ce8b498eae1390324775b72
fabriciodisalvo <fabriciodisalvo@gmail.com>
2021-02-01 22:17:54 -0300

    Create Delete Expense feature.
    Modified main.db.
    EXPENSES table now has a ExpenseStatus field that
    can be used to mark as Deleted, and probably other statuses in the
    near future.
    
    Modified app.py.
    Changed "/list" to only show expenses with no deleted status.
    Changed "/view/<expense_id>" to include ExpenseId, in order to use it to delete that id.
    Added "/delete/<expense_id>" to act as a confirmation page:
    Added "/deleted/<expense_id>" to actually mark the item as Deleted in ExpenseStatus, and
    to act as a confirmation of Deletion.
    
    Modified templates/expense_list.html.
    Table now pulls Status from database instead of being a placeholder.
    
    Modified templates/expense_view.html.
    Added DELETE button to view. Rerouts to "/delete/<expense_id>"
    
    Added templates/expense_delete.html.
    Mimics VIEW, but it acts as a confirmation page for deletion.
    Rerouts to "/deleted/<expense_id>"
    
    Added templates/expense_deleted.html.
    Sends confirmation to app.py to mark expense as deleted.

--- app.py
+++ app.py
@@ -23,22 +23,41 @@ def index():
 # Create List Expenses site
 @app.route('/list')
 def list_expense():
-    query = "SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount, ExpenseId FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId ORDER BY ExpenseDate"
+    query = "SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount, ExpenseStatus, ExpenseId FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId  WHERE ExpenseStatus<>'Deleted' ORDER BY ExpenseDate"
     table_list_data = cursor.execute(query)
     return render_template('expense_list.html', title='Expenses', table_list_data=table_list_data)
 
 # Create View Single Expenses site,
 # as per https://pythonise.com/series/learning-flask/generating-dynamic-urls-with-flask
 @app.route("/view/<expense_id>")
-def profile(expense_id):
+def view(expense_id):
     query = f"SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount, ExpenseId FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId WHERE ExpenseId={expense_id}"
     table_list_data = cursor.execute(query)
     for data in table_list_data:
-        expense_data = [data[0], data[1], data[2], data[3], format_amount_view_expense(data[4])]
+        expense_data = [data[0], data[1], data[2], data[3], format_amount_view_expense(data[4]), data[5]]
         break
         # Falta crear una funcion que traiga los tres gastos mas relevantes para mostrar debajo del gasto principal...
     return render_template("expense_view.html", expense_data=expense_data)
 
+# Create Delete Single Expenses site
+@app.route("/delete/<expense_id>")
+def delete(expense_id):
+    query = f"SELECT ExpenseDate, ExpenseDescription, CategoryName, ACCOUNTS.AccountName, ExpenseAmount, ExpenseId FROM EXPENSES JOIN ACCOUNTS ON EXPENSES.AccountId=ACCOUNTS.AccountId JOIN CATEGORIES ON EXPENSES.CategoryId=CATEGORIES.CategoryId WHERE ExpenseId={expense_id}"
+    table_list_data = cursor.execute(query)
+    for data in table_list_data:
+        expense_data = [data[0], data[1], data[2], data[3], format_amount_view_expense(data[4]), data[5]]
+        break
+    return render_template("expense_delete.html", expense_data=expense_data)
+
+# Create Deleted Expenses placeholder
+@app.route("/deleted/<expense_id>")
+def deleted(expense_id):
+    query = f"UPDATE EXPENSES SET ExpenseStatus= 'Deleted' WHERE ExpenseId={expense_id};"
+    table_list_data = cursor.execute(query)
+    connection.commit()
+    return render_template("expense_deleted.html")
+
+# Create Add Single Expense site
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "GET":
